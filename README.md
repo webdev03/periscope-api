99fa3970799789114ed6dec18e8213bc5bfcd28e
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-04-20 12:33:57 +1200

    basics

---
+++ README.md
@@ -0,0 +1,2 @@
+# periscope-api
+Note: I did not make Periscope. @ILOVETOMAKESTUFF on github made it. This is just the API/proxy for periscope.
---
+++ api/getUserData.js
@@ -0,0 +1,21 @@
+import fetch from "cross-fetch";
+
+export default async function handler(request, response) {
+  // The user
+  const { user } = request.query;
+
+  const messageCount = await (
+    await (
+      await fetch(`https://api.scratch.mit.edu/users/${user}/messages/count`, {
+        headers: {
+          "User-Agent": "Mozilla/5.0",
+        },
+      })
+    ).json()
+  ).count;
+
+  response.status(200).json({
+    username: user,
+    messages: messageCount
+  });
+}
