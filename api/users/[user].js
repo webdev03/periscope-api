a3d46a07e0ce116f9942d58f933f9863d3710fbd
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-04-20 13:24:21 +1200

    add more data

--- api/users/[user].js
+++ api/users/[user].js
@@ -3,6 +3,22 @@ import fetch from "cross-fetch";
 export default async function handler(request, response) {
   // The user
   const { user } = request.query;
+
+  const apiDataFetch = await fetch(
+    `https://api.scratch.mit.edu/users/${user}/messages/count`,
+    {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+      },
+    }
+  );
+
+  if (!apiDataFetch.ok) {
+    response.status(404);
+  }
+
+  const apiData = await apiDataFetch.json();
+
   const messageCount = await (
     await (
       await fetch(`https://api.scratch.mit.edu/users/${user}/messages/count`, {
@@ -14,7 +30,10 @@ export default async function handler(request, response) {
   ).count;
 
   response.status(200).json({
-    username: user,
-    messages: messageCount
+    id: apiData.id,
+    username: apiData.username,
+    scratchteam: apiData.scratchteam,
+    profile: apiData.profile,
+    messages: messageCount,
   });
 }
