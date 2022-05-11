22aca1f38c20f86e289167eb304c63a8f6a441f1
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-05-11 15:46:44 +1200

    add basic studio api

---
+++ api/studios/[studio].js
@@ -0,0 +1,25 @@
+import fetch from "cross-fetch";
+
+export default async function handler(request, response) {
+  // The user
+  const { studio } = request.query;
+
+  const apiDataFetch = await fetch(
+    `https://api.scratch.mit.edu/studios/${studio}/`,
+    {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+      },
+    }
+  );
+
+  if (!apiDataFetch.ok) {
+    response.status(404).json({
+      error: true
+    });
+  }
+
+  const apiData = await apiDataFetch.json();
+
+  response.status(200).json(apiData);
+}
